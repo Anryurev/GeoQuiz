e560bad88c4df1d6b24fc951688a8c775f9f06fd
Anryurev <137984074+Anryurev@users.noreply.github.com>
2023-11-06 23:16:46 +0700

    Добавим ключ для дополнения (CheatActivity.kt)

--- app/src/main/java/com/bignerdranch/android/geomain/CheatActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geomain/CheatActivity.kt
@@ -3,6 +3,8 @@ package com.bignerdranch.android.geomain
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+private const val EXTRA_ANSWER_IS_TRUE =
+    "com.bignerdranch.android.geoquiz.answer_is_true"
 class CheatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
