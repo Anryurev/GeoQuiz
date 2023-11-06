317dfd2012e6fde9af6c984b2b73d8aaf70bbe0e
Anryurev <137984074+Anryurev@users.noreply.github.com>
2023-11-06 23:19:20 +0700

    Функция newIntent(...) для CheatActivity

--- app/src/main/java/com/bignerdranch/android/geomain/CheatActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geomain/CheatActivity.kt
@@ -1,5 +1,7 @@
 package com.bignerdranch.android.geomain
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -9,5 +11,13 @@ class CheatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cheat)
+    }
+
+    companion object {
+        fun newIntent(packageContext: Context, answerIsTrue: Boolean): Intent {
+            return Intent(packageContext, CheatActivity::class.java).apply {
+                putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue)
+            }
+        }
     }
 }
